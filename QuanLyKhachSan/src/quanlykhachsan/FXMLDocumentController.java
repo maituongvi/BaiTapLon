56dbd481d72b590a6a35aee948f15753e9429616
maituongvi <maituongvi169@gmail.com>
2019-10-30 10:18:54 +0700

    update1
    Up date lần đầu tiên file quản lý khách sạn và cơ sở dữ liệu

---
+++ QuanLyKhachSan/src/quanlykhachsan/FXMLDocumentController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlykhachsan;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+
+/**
+ *
+ * @author HP
+ */
+public class FXMLDocumentController implements Initializable {
+    @FXML
+    private AnchorPane base;
+    
+   
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+    } 
+   
+    
+}
